ad8a6e11d24f59cc0c51fa9d7277dd31e43444a0
Masatoshi <msts.oo0131@gmail.com>
2021-02-02 22:16:09 +0900

    learned_basic.css

--- src/App.js
+++ src/App.js
@@ -13,13 +13,23 @@ import  BindingHandler from "./components/BindingHandler"
 // import ChildrenComponent from "./components/Children";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import './appStyles.css';
+import styles from  './appStyles.module.css'
+
+
 
 
 class App extends Component{
     render() {
       return(
        <div className="App">
-           <NameList/>
+           <h1 className='error'>Error</h1>
+           <h1 className={styles.success}>Success</h1>
+           <Inline/>
+           {/*<Stylesheet primary={ false }/>*/}
+           {/*<NameList/>*/}
            {/*<BindingHandler/>*/}
            {/*<UserGreeting/>*/}
            {/*<Parent/>*/}
