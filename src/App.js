54d43246043fafc845b02c9e38eee25755aab284
Masatoshi <msts.oo0131@gmail.com>
2021-10-17 16:15:07 +0900

    props_add_function_use

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react'
 import './App.css'
 import Greet from './components/Greet'
 import Welcome from "./components/Welcome";
+import {Link} from "@material-ui/core";
+
 
 class App extends Component{
     render() {
       return(
        <div className="App">
-         <Greet/>
-         <Welcome/>
+         <Greet name="props"/>
+           {/*<Welcome/>*/}
+         <Link> Fuck you </Link>
        </div>
       );
     }
 }
 
 export default App;
+
+//古くなったプロジェクトにおいてのgit clone をする際は、package.jsonのverをremoteからインストールを行い
+//yarn install or npm installを行う。
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,16 @@
 import React from  'react'
 
-function Greet() {
-    return <h1>  Hello World ! </h1>
+
+const Greet = props => {
+
+    return <h1>  Hello {props.name} ! </h1>
 }
 
 export default Greet;
+
+
+
+// 親クラス(App.js)で定義された変数(プロパティ)を子(Componentsファイル群にて)で継承をする。
+// その際,{props.children}と定義してあげることで不確定要素等を全て親ファイルからの継承で呼び出しが可能になる。
+// (そうする場合は必ず<>タグにてラッピングしてあげないとエラるから注意, => <div></div> {props._children}とはならない)
+// ただ、親クラスでのformの入力を行う(Form等)ことで子に渡せると思うがどのようにしてその渡し方を行うかが重要になる。
