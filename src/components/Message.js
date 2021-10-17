6317bd767afad57cca8a17951e0e915674b35e09
Masatoshi <msts.oo0131@gmail.com>
2021-10-17 16:37:59 +0900

    state_function_comment.add

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,7 +5,7 @@ class Message extends Component {
     constructor() {
         super()
         this.state= {
-            message: 'Welcome haha!'
+            message: 'fuck you!'
         }
     }
 
@@ -18,7 +18,7 @@ class Message extends Component {
     render() {
       return(
           <div>
-              <h1>{ this.state.message}</h1>
+              <h1>{this.state.message}</h1>
               <button onClick={ ()=> this.changeMessage()}> click </button>
           </div>
       )
@@ -26,3 +26,7 @@ class Message extends Component {
 }
 
 export default Message;
+
+//
+// this.stateにて初期値を代入する。→その後this.setStateを利用し,そのmessageを更新するようになっている。
+// ただ、state自体の値はボタンを押すことで変わっている為,再度ボタンを押しても最初の値である(this.state)の状態には戻らない。
