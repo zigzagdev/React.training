721b5359d6de96eb00585bff8e1265e881e8a096
zigzagdev <msts.oo0131@gmail.com>
2022-07-31 17:08:18 +0900

    Add_explain_difference

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Welcome = (props) => {
-  console.log(props);
-  return <h2> Welcome {props.name} to {props.hero}</h2>
+
+class Welcome extends Component {
+    render(){
+     return(
+      <h2>
+        Welcome {this.props.name} to {this.props.name}
+      </h2>
+     )
+    }
 }
 
-export default Welcome;
+// const Welcome = (props) => {
+//   console.log(props);
+//   return <h2> Welcome {props.name} to {props.hero}</h2>
+// }
+//
+// export default Welcome;
 
 
 //基本的に関数コンポでの書くことが多い。(thisやbindを使う必要性がない為。同時にシンプルな構文での作成が可能になる)
+// 故に上二つの書き方は全く同じである。
