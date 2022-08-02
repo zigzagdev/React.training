aa4a61d781b8f6093072caf4b56963693a5c85b6
zigzagdev <msts.oo0131@gmail.com>
2022-08-03 01:20:13 +0900

    fix_welcomePage

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,14 +1,12 @@
 import React from  'react'
 
 
-const Greet = props => {
-
+const Greet = (props) => {
+    
     return <h1>  Hello {props.name} ! </h1>
 }
 
 export default Greet;
-
-
 
 // 親クラス(App.js)で定義された変数(プロパティ)を子(Componentsファイル群にて)で継承をする。
 // その際,{props.children}と定義してあげることで不確定要素等を全て親ファイルからの継承で呼び出しが可能になる。
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Welcome extends Component{
-    render() {
-     return(
-         <h1> Class Component </h1>
-     )
-    }
+const Welcome = (props) => {
+  console.log(props)
+  return(
+    <div>
+      This is Welcome page.
+    </div>
+  )
 }
 
 export default Welcome;
