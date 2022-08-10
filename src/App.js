a700b98c9470428056e57b4c86d23fec990eb158
zigzagdev <msts.oo0131@gmail.com>
2022-08-10 15:04:21 +0900

    ClassCompo_changeg_FuncCompo

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import './App.css'
 import Greet from './components/Greet'
 import Welcome from "./components/Welcome";
 
-
-class App extends Component{
-    render() {
-      return(
-       <div className="App">
-         <Greet name="Fuck you" age="45"/>
-         <Welcome/>
-       </div>
-      );
-    }
+const App = () => {
+  return(
+    <div className="App">
+      <Greet name="Fuck you" age="45"/>
+      <Welcome/>
+    </div>
+  )
 }
 
 export default App;
