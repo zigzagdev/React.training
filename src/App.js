104665c54612e712018b0bc207ffc1c39f90d5e1
zigzagdev <msts.oo0131@gmail.com>
2023-01-27 23:01:13 +0900

    developBranch_clean

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import './App.css'
-import Greet from './components/Greet'
-import Welcome from "./components/Welcome";
-import Count from "./components/Counter"
 
 const App = () => {
   return(
     <div className="App">
-      <Greet name="Fuck you" age="45"/>
-      <Welcome/>
-      <Count/>
+        Hello world !!
     </div>
   )
 }
