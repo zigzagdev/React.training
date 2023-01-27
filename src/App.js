e78964b69c929acb86d9db09ca61be0e0386aefe
zigzagdev <msts.oo0131@gmail.com>
2023-01-27 23:05:57 +0900

    developBranch_clean

--- src/App.js
+++ src/App.js
@@ -9,3 +9,8 @@ const App = () => {
   )
 }
 export default App;
+
+// How To fetch remote branch.
+
+// 1. use command git branch -a and checkout to what you want.
+// 2. use command git checkout -b (first argument is your local branch name and second one is remote branch )
