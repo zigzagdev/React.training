88df8571f8d9130d35fc4eafacf1a425409c4758
zigzagdev <msts.oo0131@gmail.com>
2023-02-19 00:18:41 +0900

    addJsFile

---
+++ src/Apptest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Apptest(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Apptest;
---
+++ src/Jest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jest = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Jest;
